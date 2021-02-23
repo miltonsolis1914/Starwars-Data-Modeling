f8ccc0da91475f2f3bfbd6e1fb81b0c69a117b24
miltonsolis1914 <miltonsolis26@gmail.com>
2021-02-23 21:53:52 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,40 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class user(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(200), nullable=False)
+    favorites = Column(String(100))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class planet_cards(Base):
+    __tablename__ = 'planet_cards'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name_planets = Column(String(200))
+    description = Column(String(200))
+    population = Column(String(200))
+    climate = Column(String(200))
+    terrain = Column(String(200))
+    rotation_period = Column(String(200))
 
-    def to_dict(self):
-        return {}
+
+class name_characters(Base):
+    __tablename__ = 'name_characters'
+    id = Column(Integer, primary_key=True)
+    name_characters = Column(String(200))
+    descriptor = Column(String(200))
+    gender = Column(String(200))
+    hair_color = Column(String(200))
+    skin_color = Column(String(200))
+    birth_year = Column(String(200))
+    
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(200), ForeignKey('user.id'))
+    planet_cards = Column(String(200), ForeignKey('planet_cards.id'))
+    name_characters = Column(String(200), ForeignKey('name_characters.id'))
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
